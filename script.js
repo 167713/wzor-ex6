c29f3dd3e83dea3cd1dce1b079680a22251afbd7
167713 <100697872+167713@users.noreply.github.com>
2022-06-06 11:10:30 +0000

    Działa commit1 i commit2

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 (function () {
-  //TODO
+  const regexpCoordinates = /\W/;
+  ex1_button.addEventListener("click", function(){
+  ex1_content.innerHTML = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+  })
+  ex2_text.addEventListener("input", function(){
+     if(isNaN(ex2_text.value)){
+       if(ex2_text.value.match(regexpCoordinates))
+        ex2_content.innerHTML = "Numer nie może zawierać znaków specjalnych!";
+       else
+        ex2_content.innerHTML = "Numer nie może zawierać liter!";
+     }
+    else if(ex2_text.value.length != 9)
+      ex2_content.innerHTML = "Długość numeru musi być równa 9!";
+    else
+      ex2_content.innerHTML = "Numer telefonu jest poprawny.";
+  })
 })();
